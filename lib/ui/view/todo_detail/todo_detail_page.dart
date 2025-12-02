3eb23e298819e3d16edcd76ca3d77190dc86aaab
hailey <ahhh0325@gmail.com>
2025-12-02 11:05:35 +0900

    feat: TodoDetailPage 에 todo 수정 기능 추가

--- lib/ui/view/todo_detail/todo_detail_page.dart
+++ lib/ui/view/todo_detail/todo_detail_page.dart
@@ -1,10 +1,12 @@
+import 'package:assignment4/logic/entity/todo.dart';
 import 'package:assignment4/ui/provider/todo_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:assignment4/core/app_theme.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tap_debouncer/tap_debouncer.dart';
 
-class TodoDetailPage extends ConsumerWidget {
+class TodoDetailPage extends HookConsumerWidget {
   const TodoDetailPage({super.key, required this.id});
   final String id;
 
@@ -13,51 +15,127 @@ class TodoDetailPage extends ConsumerWidget {
     // TodoViewModel 구독
     final todo = ref.watch(todoViewModelProvider).firstWhere((t) => t.id == id);
 
+    // Flutter Hooks 로 지역 상태 관리
+    final isEditing = useState(false);
+
+    // 사용자 입력값 받기
+    final titController = useTextEditingController();
+    final desController = useTextEditingController();
+
+    // 기존 입력값 불러오기
+    useEffect(() {
+      titController.text = todo.title;
+      desController.text = todo.description ?? '';
+      return;
+    }, [titController, desController]);
+
+    // 수정 함수
+    void editToDo() {
+      final title = titController.text.trim();
+      // 제목 입력값 있을 때
+      if (title.isNotEmpty) {
+        ref
+            .read(todoViewModelProvider.notifier)
+            .updateTodo(
+              Todo(
+                id: todo.id,
+                title: titController.text,
+                description: desController.text,
+                isFavorite: todo.isFavorite,
+                isDone: todo.isDone,
+                createdAt: todo.createdAt,
+              ),
+            );
+        isEditing.value = false;
+        return;
+        // 제목 입력값 없을 때
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              '할 일을 입력해주세요.',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: fxc(context).brandColor,
+            behavior: SnackBarBehavior.floating,
+            duration: const Duration(seconds: 2),
+          ),
+        );
+      }
+    }
+
     return Scaffold(
       backgroundColor: vrc(context).background300,
       appBar: AppBar(
         backgroundColor: vrc(context).background200,
         scrolledUnderElevation: 0,
         centerTitle: true,
+        actionsPadding: EdgeInsets.only(right: 5),
         title: Hero(
           tag: '${todo.id}',
           child: Material(
             color: Colors.transparent,
             // 제목
-            child: Text(
-              todo.title,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                color: vrc(context).textColor200,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            child: isEditing.value
+                ? TextField(
+                    onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                    controller: titController,
+                    style: TextStyle(fontSize: 20),
+                    autofocus: true,
+                    cursorColor: fxc(context).brandColor,
+                    decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: fxc(context).brandColor!,
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                    onSubmitted: (value) => editToDo(), // 수정 함수 실행
+                  )
+                : Text(
+                    todo.title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: vrc(context).textColor200,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
           ),
         ),
         actions: [
+          // 수정 버튼
+          TapDebouncer(
+            onTap: () async {
+              if (isEditing.value) {
+                editToDo();
+                isEditing.value = !isEditing.value;
+              }
+              isEditing.value = !isEditing.value;
+            },
+
+            builder: (BuildContext context, TapDebouncerFunc? onTap) {
+              return IconButton(
+                onPressed: onTap,
+                icon: Icon(isEditing.value ? Icons.done : Icons.edit),
+              );
+            },
+          ),
+
           // 즐겨찾기 버튼
           TapDebouncer(
             onTap: () async => await ref
                 .read(todoViewModelProvider.notifier)
                 .toggleFavorite(todo),
             builder: (BuildContext context, TapDebouncerFunc? onTap) {
-              return InkWell(
-                onTap: onTap,
-                child: Container(
-                  margin: const EdgeInsets.only(right: 8),
-                  width: 48,
-                  height: 48,
-                  child: Center(
-                    child: Icon(
-                      todo.isFavorite
-                          ? Icons.star_rounded
-                          : Icons.star_border_rounded,
-                      size: 30,
-                      color: vrc(context).textColor200,
-                    ),
-                  ),
+              return IconButton(
+                onPressed: onTap,
+                icon: Icon(
+                  todo.isFavorite
+                      ? Icons.star_rounded
+                      : Icons.star_border_rounded,
                 ),
               );
             },
@@ -81,13 +159,29 @@ class TodoDetailPage extends ConsumerWidget {
             ),
             // 세부정보
             Expanded(
-              child: Text(
-                todo.description ?? '세부정보',
-                style: TextStyle(
-                  color: vrc(context).textColor200,
-                  fontSize: 16,
-                ),
-              ),
+              child: isEditing.value
+                  ? TextField(
+                      onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                      controller: desController,
+                      style: TextStyle(fontSize: 16),
+                      maxLines: 50,
+                      cursorColor: fxc(context).brandColor,
+                      decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: fxc(context).brandColor!,
+                            width: 2,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Text(
+                      todo.description ?? '세부정보',
+                      style: TextStyle(
+                        color: vrc(context).textColor200,
+                        fontSize: 16,
+                      ),
+                    ),
             ),
           ],
         ),
